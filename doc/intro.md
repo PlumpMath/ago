52500e2ada6d59254a8893db7015963cb11408af
Steve Yen <steve.yen@gmail.com>
2014-04-15 14:52:21 -0700

    First commit.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to rewindable
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/rewindable/core_test.clj
@@ -0,0 +1,7 @@
+(ns rewindable.core-test
+  (:require [clojure.test :refer :all]
+            [rewindable.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
