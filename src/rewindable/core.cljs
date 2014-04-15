ed89f95e0ee15da3901f7d6b80a8e274ac6086dc
Steve Yen <steve.yen@gmail.com>
2014-04-15 15:11:49 -0700

    Listen to clicks.

--- src/rewindable/core.cljs
+++ src/rewindable/core.cljs
@@ -1,13 +1,25 @@
 (ns rewindable.core
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [clojure.string :as string]
-            [cljs.core.async :refer [chan <! >! close! timeout merge
+            [cljs.core.async :refer [chan <! >! put! close! timeout merge
                                      map< filter< sliding-buffer]]
-            [goog.dom :as gdom]))
+            [goog.dom :as gdom]
+            [goog.events :as gevents]))
 
 (enable-console-print!)
+
+(defn listen-el [el type]
+  (let [out (chan)]
+    (gevents/listen el type #(put! out %))
+    out))
 
 (defn hello []
   (println "Hello, World!"))
 
 (hello)
+
+(let [hi-ch (listen-el (gdom/getElement "hi") "click")]
+  (go (loop [num-hi 0]
+        (<! hi-ch)
+        (println "num-hi" num-hi)
+        (recur (inc num-hi)))))
