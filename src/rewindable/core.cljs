8f2ce6737d03b4556520f98a1fc85ba4dc547fc5
Steve Yen <steve.yen@gmail.com>
2014-04-18 12:35:26 -0700

    Added placeholder save world button.

--- src/rewindable/core.cljs
+++ src/rewindable/core.cljs
@@ -1,6 +1,6 @@
 (ns rewindable.core
   (:require-macros [rewindable.ago-macros :refer [my-go]])
-  (:require [cljs.core.async :refer [chan <! put!]]
+  (:require [cljs.core.async :refer [chan <! !> alts! put!]]
             [rewindable.ago :refer [make-ago-world ago-world-chan]]
             [goog.dom :as gdom]
             [goog.events :as gevents]))
@@ -24,11 +24,12 @@
            msg)))
 
 (let [hi-ch (listen-el (gdom/getElement "hi") "click")
+      stw-ch (listen-el (gdom/getElement "stw") "click") ; save-the-world button
       agw (make-ago-world)
       ch1 (ago-world-chan agw 1)]
   (my-go agw
          (loop [num-hi 0]
-           (let [x (<! hi-ch)]
+           (let [[x ch] (alts! [hi-ch stw-ch])]
              (println "num-hi" num-hi)
              (>! ch1 [num-hi x])
              (println :agw @agw)
