ae1a27ea54d35f10a1e56fbb21dc149b2de81e08
Steve Yen <steve.yen@gmail.com>
2014-04-24 07:36:19 -0700

    Put hooks after plugins.

--- project.clj
+++ project.clj
@@ -4,17 +4,14 @@
   :scm {:url "git@github.com:steveyen/ago.git"}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2138"]
                  [org.clojure/core.async "0.1.278.0-76b25b-alpha"]]
-
-  :hooks [leiningen.cljsbuild]
   :plugins [[lein-cljsbuild "1.0.1"]
             [com.cemerick/clojurescript.test "0.3.0"]]
+  :hooks [leiningen.cljsbuild]
 
   :source-paths ["src"]
-
   :clojurescript? true
   :cljsbuild {
     :builds [{:id "ago"
