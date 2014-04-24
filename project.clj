20e0d5b672a6ac16c45c335f4c8be2a22a8dc57b
Steve Yen <steve.yen@gmail.com>
2014-04-23 18:00:13 -0700

    More info in project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject ago "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-
+  :description "snapshoting and rewinding built on top of clojurescript core.async"
+  :url "https://github.com/steveyen/ago"
+  :scm {:url "git@github.com:steveyen/ago.git"}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
@@ -9,10 +9,13 @@
                  [org.clojure/clojurescript "0.0-2138"]
                  [org.clojure/core.async "0.1.278.0-76b25b-alpha"]]
 
-  :plugins [[lein-cljsbuild "1.0.1"]]
+  :hooks [leiningen.cljsbuild]
+  :plugins [[lein-cljsbuild "1.0.1"]
+            [com.cemerick/clojurescript.test "0.3.0"]]
 
   :source-paths ["src"]
 
+  :clojurescript? true
   :cljsbuild {
     :builds [{:id "ago"
               :source-paths ["src"]
