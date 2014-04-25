1e45e500022acfef03cf2d75f41bd2c700fb4eff
Steve Yen <steve.yen@gmail.com>
2014-04-24 19:00:04 -0700

    Added test-app-data.

--- test-phantomjs/ago/test-phantomjs.cljs
+++ test-phantomjs/ago/test-phantomjs.cljs
@@ -1,12 +1,15 @@
-; This ago.unit-test script is meant to run headless, not in a browser,
-; but instead in something like phantomjs.
+; This ago.unit-test script is meant to run headless in phantomjs.
 
 (ns ago.test-phantomjs
-  (:require [ago.core]))
+  (:require [ago.core :refer [make-ago-world]]))
 
 (def success 0)
 
+(defn test-app-data []
+  (assert (not= nil (make-ago-world nil)))
+  (assert (= (:app-data @(make-ago-world :my-app-data)) :my-app-data)))
+
 (defn ^:export run []
-  (.log js/console "ago unit test started.")
-  (.log js/console (+ 1 2))
+  (.log js/console "ago test-phantomjs started.")
+  (test-app-data)
   success)
